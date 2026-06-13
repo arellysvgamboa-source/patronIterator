5383979e1905dd930eba9a76569d45e21242a707
Arellys Villena <arellysvgamboa@gmail.com>
2026-06-13 12:43:26 -0500

    feat: agregar iterador de pagos ordenado por prioridad financiera

--- src/iterator/PagoPorPrioridad.java
+++ src/iterator/PagoPorPrioridad.java
@@ -1,4 +1,43 @@
 package iterator;
+import modelo.Pago;
+import java.util.*;
 
-public class PagoPorPrioridad {
+public class PagoPorPrioridad implements IteratorPago {
+
+    private ArrayList<Pago> pagosOrdenados;
+    private int posicion;
+
+    public PagoPorPrioridad(ArrayList<Pago> pagos) {
+        pagosOrdenados = new ArrayList<>();
+
+        for (Pago p : pagos) {
+            if (p.getEstado().equalsIgnoreCase("Vencido")) {
+                pagosOrdenados.add(p);
+            }
+        }
+
+        for (Pago p : pagos) {
+            if (p.getEstado().equalsIgnoreCase("Pendiente")) {
+                pagosOrdenados.add(p);
+            }
+        }
+
+        for (Pago p : pagos) {
+            if (p.getEstado().equalsIgnoreCase("Pagado")) {
+                pagosOrdenados.add(p);
+            }
+        }
+
+        posicion = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return posicion < pagosOrdenados.size();
+    }
+
+    @Override
+    public Pago next() {
+        return pagosOrdenados.get(posicion++);
+    }
 }
